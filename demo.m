3dd4139a5db283e017eef4ef3c534cba81c18cfc
Boubat <mboubat@gmail.com>
2020-09-11 21:17:34 +0200

    refactor: Update demo.

--- demo.m
+++ demo.m
@@ -1,115 +1,131 @@
-maxIteration = 6000;
+greetings();
+UpdatableTextDemo();
+ProgressionDisplayDemo();
 
-
-
-greetings = UpdatableText;
-welcomeMessage = 'This demo is about to start';
-for i = 1:length(welcomeMessage)
-  greetings.print(welcomeMessage(1:i));
-  pause(0.05);
-end
-pause(1);
-for i = 1:length(welcomeMessage)
-  greetings.print(welcomeMessage(1:end-i));
-  pause(0.05);
-end
-welcomeMessage = {'Let''s' 'go'};
-for i = 1:length(welcomeMessage)
-  greetings.print(welcomeMessage{i});
-  pause(0.4);
-end
-greetings.erase();
-
-
-
-
-elapsedTime = UpdatableText('Elapsed time: ',' milliseconds.');
-tic
-for i = 1:maxIteration
-  elapsedTime.print(num2str(i));
-  pause(0.001);
-end
-toc
-fprintf('\n')
-
-
-
-progression = UpdatableText('',' Progression in percent.');
-tic
-for i = 1:maxIteration
-  progression.printPercent(i/maxIteration);
-  pause(0.001);
-end
-toc
-fprintf('\n')
-
-
-
-progression = UpdatableText('',' Progression with default progress bar.');
-tic
-for i = 1:maxIteration
-  progression.printProgressBar(i/maxIteration);
-  pause(0.001);
-end
-toc
-fprintf('\n')
-
-
-
-progression = UpdatableText('',' Progression in percent and a 5 characters long progress bar.');
-tic
-for i = 1:maxIteration
-  progression.printPercent(i/maxIteration);
-  progression.printProgressBar(i/maxIteration,5);
-  pause(0.001);
-end
-toc
-fprintf('\n')
-
-
-
-progression = UpdatableText('',' Progression in percent and a 50 characters long progress bar.');
-tic
-for i = 1:maxIteration
-  progression.printPercent(i/maxIteration);
-  progression.printProgressBar(i/maxIteration,50);
-  pause(0.001);
-end
-toc
-fprintf('\n')
-
-
-
-progression = UpdatableText('',' Progression in percent, default progress bar and special iteration printed out.');
-tic
-for i = 1:maxIteration
-  progression.printPercent(i/maxIteration);
-  progression.printProgressBar(i/maxIteration);
-  if not(mod(i,1000))
-    progression.printAbove(sprintf('Step %d completed',i));
+function greetings()
+  greetings = UpdatableText;
+  welcomeMessage = 'This demo is about to start';
+  for i = 1:length(welcomeMessage)
+    greetings.print(welcomeMessage(1:i));
+    pause(0.05);
   end
-  pause(0.001);
-end
-toc
-fprintf('\n')
-
-
-
-outerProgression = UpdatableText('Outer loop ');
-innerProgression = UpdatableText('Inner loop ');
-outerProgression.print('0/6 ');
-outerProgression.printProgressBar(0,6);
-tic
-for i = 1:6
-  for j = 1:100
-    innerProgression.print(sprintf('step %d ',i));
-    innerProgression.printPercent(j/100);
-    pause(0.01);
+  pause(1);
+  for i = 1:length(welcomeMessage)
+    greetings.print(welcomeMessage(1:end-i));
+    pause(0.05);
   end
-  innerProgression.erase();
-  outerProgression.print(sprintf('%d/%d ',i,6));
-  outerProgression.printProgressBar(i/6,6);
-  innerProgression.print();
+  greetings.erase();
 end
-toc
-fprintf('\n')
+
+function UpdatableTextDemo()
+  fprintf('Let''s try the UpdatableText class:\n\n');
+  pause(2);
+
+  demoText = UpdatableText;
+  textsToShow = {'This','is','a','basic','updatable','text'};
+  for text = textsToShow
+    demoText.print(text{1});
+    pause(0.6);
+  end
+  
+  demoText.setPrefix('PREFIX ');
+  demoText.print('now with a prefix');
+  pause(2.5);
+  
+  demoText.setSuffix(' SUFFIX\n');
+  demoText.print('now with a suffix');
+  pause(2.5);
+
+  demoText.erase();
+  demoText = UpdatableText('PREFIX ',' SUFFIX\n');
+  demoText.print('prefix and suffix can be given to the UpdatableText constructor');
+  pause(5);
+  
+  demoText.setPrefix('PREFIX');
+  demoText.setSuffix('SUFFIX');
+  demoText.setFormatString('_prefix _text _suffix\n');
+  demoText.print('you can change the format string');
+  pause(2.5);
+  demoText.setFormatString('_suffix _text _prefix\n');
+  pause(1);
+  demoText.setFormatString('_prefix _text _suffix\n');
+  pause(1);
+  demoText.setFormatString('_suffix _text _prefix\n');
+  pause(1);
+  demoText.setFormatString('_prefix _text _suffix\n');
+  pause(1);
+  demoText.print('like you want');
+  pause(2.5);
+  demoText.setFormatString('_text _prefix _suffix\n');
+  pause(1);
+  demoText.setFormatString('_text _prefix _suffix_suffix\n');
+  pause(1);
+  demoText.setFormatString('_text _prefix_prefix _suffix_suffix\n');
+  pause(1);
+  demoText.setFormatString('_text _text _prefix_prefix _suffix_suffix\n');
+  pause(1);
+  demoText.setFormatString('_text _text        _prefix_prefix _suffix_suffix\n');
+  pause(1);
+  demoText.setFormatString('_prefix _text _suffix\n');
+  demoText.print('back to normal');
+  pause(2.5);
+
+  demoText.print('you can also print lines above');
+  pause(2.5);
+  demoText.printAbove('This line has been printed above...');
+  pause(2.5);
+  demoText.printAbove('... and another line printed above.');
+  pause(2.5);
+  demoText.erase();
+  fprintf('End of basic UpdatableText demonstration.\n');
+  pause(3);
+end
+
+function ProgressionDisplayDemo()
+  fprintf('\nLet''s try the ProgressionDisplay class:\n\n');
+  pause(2);
+  
+  progression = ProgressionDisplay;
+  
+  progression.print('Showing steps:');
+  progression.startPrintingStep();
+  demoLoop(progression);
+  
+  progression.print('Showing percent:');
+  progression.stopPrintingStep();
+  progression.startPrintingPercent();
+  demoLoop(progression);
+  
+  progression.print('Showing progress bar:');
+  progression.stopPrintingPercent();
+  progression.startPrintingProgressBar();
+  demoLoop(progression);
+  
+  progression.print('Resized progress bar:');
+  progression.setProgressBarLength(50);
+  demoLoop(progression);
+  progression.setProgressBarLength(20);
+  
+  progression.print('Showing all:');
+  progression.startPrintingPercent();
+  progression.startPrintingStep();
+  demoLoop(progression);
+  
+  progression.setProgressBarLength(4);
+  progression.print('Playing with format string:');
+  progression.setFormatString('_text _step _progressbar_percent_progressbar_percent_progressbar_percent_progressbar_percent_progressbar\n')
+  demoLoop(progression);
+  pause(2.5);
+
+  progression.erase();
+  fprintf('End of ProgressionDisplay demonstration.\n');
+end
+
+function demoLoop(progressionDisplay)
+  maxStep = 400;
+  pauseLength = 0.01;
+  for i = 0:maxStep
+    progressionDisplay.updateStep(i,maxStep);
+    pause(pauseLength);
+  end
+end
